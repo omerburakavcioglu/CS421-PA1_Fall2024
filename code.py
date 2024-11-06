6b9ce25c54244b778a960b50d59dcdbe55a33f37
Ömer Burak Avcıoğlu <133652808+omerburakavcioglu@users.noreply.github.com>
2024-11-06 11:30:13 +0300

    Updated code.py content

--- code.py
+++ code.py
@@ -1,7 +1,19 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "22002849 oo yeah"
+    return "22002849"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
